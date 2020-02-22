f77b28312602a394dfb18b92eaaae54d460a59f0
sjilss <41061396+sjilss@users.noreply.github.com>
2020-02-22 11:53:23 +0800

    Update extract.bat

--- extract.bat
+++ extract.bat
@@ -1,1 +1,1 @@
-ldecod.exe -p InputFile=walk_cif_stego.264 -p OutputFile=walk_stego_dec.yuv -M ext-meg.txt -T messagetemp.txt
+ext-PTC.exe -p InputFile=walk_cif_stego.264 -p OutputFile=walk_stego_dec.yuv -M ext-meg.txt -T messagetemp.txt
